82e9a89561d9ffe107f49bb4b464c501905317c4
georgechiu <georgechiu@palsys.com.tw>
2023-10-03 18:11:13 +0800

    add zero downtime

--- Db.cs
+++ Db.cs
@@ -10,7 +10,7 @@ namespace PizzaStore.DB;
  {
    private static List<Pizza> _pizzas = new List<Pizza>()
    {
-     new Pizza{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
+     new Pizza{ Id=1, Name="New Pizza Montemagno, Pizza shaped like a great mountain" },
      new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
      new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"} 
    };
